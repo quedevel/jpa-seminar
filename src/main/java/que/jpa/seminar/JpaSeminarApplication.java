d1e45f004135904462629d6cca802e9aa8250312
quedevel <kdh90817@naver.com>
2023-07-31 10:31:26 +0900

    Initial commit

---
+++ src/main/java/que/jpa/seminar/JpaSeminarApplication.java
@@ -0,0 +1,13 @@
+package que.jpa.seminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSeminarApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JpaSeminarApplication.class, args);
+  }
+
+}
---
+++ src/test/java/que/jpa/seminar/JpaSeminarApplicationTests.java
@@ -0,0 +1,13 @@
+package que.jpa.seminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSeminarApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
